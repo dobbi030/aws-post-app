9c703440f5d530166a19e6ccb74ff6d734569128
River is line <dmstjs7047@hanmail.net>
2025-12-17 23:24:48 +0900

    get hello world

---
+++ src/main/java/com/sparta/postapplication/domains/post/controller/HelloController.java
@@ -0,0 +1,35 @@
+package com.sparta.postapplication.domains.post.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/hello")
+@RequiredArgsConstructor
+public class HelloController {
+
+	// public final HelloService helloService;
+
+	@GetMapping
+	ResponseEntity<String> hello() {
+		return ResponseEntity.ok("hello world: es.kang");
+	}
+
+	// @PostMapping
+	// ResponseEntity<Void> create(CreatePostRequest request) {
+	// 	helloService.create(request);
+	//
+	// 	return ResponseEntity.ok().build();
+	// }
+	//
+	// @GetMapping("/{postId}")
+	// ResponseEntity<GetPostResponse> getPost(@PathVariable Long postId) {
+	// 	GetPostResponse response = helloService.getPost(postId);
+	//
+	// 	return ResponseEntity.ok(response);
+	// }
+}
