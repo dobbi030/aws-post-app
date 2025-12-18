8cf3ef72009cd4579f89ee302b8835245a4a7d95
River is line <dmstjs7047@hanmail.net>
2025-12-18 13:24:44 +0900

    feat: post constructor

--- src/main/java/com/sparta/postapplication/domains/post/domain/model/Post.java
+++ src/main/java/com/sparta/postapplication/domains/post/domain/model/Post.java
@@ -32,10 +32,12 @@ public class Post {
 	@Column
 	private String content;
 
+	private Post(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+
 	public static Post create(String title, String content) {
-		Post post = new Post();
-		post.title = title;
-		post.content = content;
-		return post;
+		return new Post(title, content);
 	}
 }
